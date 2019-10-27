17f91641be80cb6bae264fb6d501830361e50b1a
Antonios Karagiannis <antokarag@gmail.com>
2019-10-27 16:46:59 -0600

    build(webpack dev): add

---
+++ webpack.dev.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+require('dotenv').config();
+
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplatePlugin = require('html-webpack-template');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './'),
+    compress: true,
+    port: 9000,
+    https: false,
+    open: false,
+    historyApiFallback: true,
+    hot: true,
+    host: '0.0.0.0',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    disableHostCheck: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|reports)/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env'),
+      safe: true,
+      systemvars: true
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: false
+    }),
+    new HtmlWebpackPlugin({
+      title: 'spsa.church',
+      minify: false,
+      inject: false,
+      template: HtmlWebpackTemplatePlugin,
+      lang: 'en-US',
+      appMountId: 'root',
+      baseHref: '/'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
