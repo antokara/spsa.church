fa4d425a7a235dd4f7ed6996c82e55e4c6f6ea22
Antonios Karagiannis <antokarag@gmail.com>
2019-11-12 22:19:25 -0700

    test(layout1): add

---
+++ src/components/layouts/Layout1.test.tsx
@@ -0,0 +1,88 @@
+import { MockedProvider, MockedResponse } from '@apollo/react-testing';
+import {
+  getByText,
+  render,
+  RenderResult,
+  waitForElementToBeRemoved
+} from '@testing-library/react';
+import * as React from 'react';
+import * as getTheme from 'src/gql/theme/getTheme.gql';
+import { Layout1 } from './Layout1';
+
+describe('Layout1 component', () => {
+  let rr: RenderResult;
+  let node: ChildNode | null;
+
+  beforeEach(() => {
+    const mocks: MockedResponse[] = [
+      {
+        request: {
+          query: getTheme
+        },
+        result: {
+          data: {
+            theme: {
+              headerMenu: {
+                label: 'header-menu-1/label',
+                menuEntries: [
+                  {
+                    label: 'menu-entry-1/label',
+                    url: 'menu-entry-1/url'
+                  },
+                  {
+                    label: 'menu-entry-2/label',
+                    url: 'menu-entry-2/url'
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
+    ];
+
+    rr = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Layout1 />
+      </MockedProvider>
+    );
+    node = rr.container.firstChild;
+  });
+
+  describe('while loading', () => {
+    it('renders the loading indicator', () => {
+      expect(node).toMatchInlineSnapshot(`
+        <div>
+          spsa.church
+          <div>
+            loading
+          </div>
+          <div />
+        </div>
+      `);
+    });
+  });
+
+  describe('on successful load', () => {
+    beforeEach(async () => {
+      await waitForElementToBeRemoved(
+        () => getByText(rr.container, 'loading'),
+        {
+          container: rr.container
+        }
+      );
+    });
+
+    it('renders the loaded component', () => {
+      expect(node).toMatchInlineSnapshot(`
+        <div>
+          spsa.church
+          <div />
+          <div>
+            header-menu-1/label
+          </div>
+        </div>
+      `);
+    });
+  });
+});
