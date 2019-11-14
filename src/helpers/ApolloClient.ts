094b912c4a08d9988014838d3f5c39f5af34a5bb
Antonios Karagiannis <antokarag@gmail.com>
2019-11-14 16:53:38 -0700

    feat(apollo client helper): add retry link

--- src/helpers/ApolloClient.ts
+++ src/helpers/ApolloClient.ts
@@ -3,6 +3,7 @@ import { ApolloClient as Client } from 'apollo-client';
 import { ApolloLink, GraphQLRequest } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
+import { RetryLink } from 'apollo-link-retry';
 
 interface IContext {
   headers: {
@@ -23,8 +24,14 @@ const httpLink: ApolloLink = createHttpLink({
   uri: process.env.TAKESHAPE_API_URL
 });
 
+// TODO: consider replacing or using additional to this the "apollo-link-error"
+//    for controlled retry by the user after the automatic failures
+//    or for controlled retry of faster retries (instead of forced delay)
+// TODO: add test for the retry in its final form
+const retryLink: RetryLink = new RetryLink();
+
 const ApolloClient: Client<{}> = new Client({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(retryLink).concat(httpLink),
   cache: new InMemoryCache()
 });
 
