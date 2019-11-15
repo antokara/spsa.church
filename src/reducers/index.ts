38ae662e47bc6feb81fed33c7859b32eda8c2cde
Antonios Karagiannis <antokarag@gmail.com>
2019-11-15 15:38:15 -0700

    feat(index reducer): toggle autofreeze by env

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,17 @@
 import { connectRouter, RouterState } from 'connected-react-router';
+import { setAutoFreeze } from 'immer';
 import { combineReducers, Reducer } from 'redux';
 import { history } from 'src/helpers/history';
 import { TState as TLayoutState } from 'src/reducers/layout/defaultState';
 import { layout } from 'src/reducers/layout/layout';
+
+/**
+ * even though it is supposed to automatically turn on-off on dev/prod,
+ * still, we should explicitly enable it for anything other than prod, (ie. dev/test/etc.)
+ *
+ * @see https://immerjs.github.io/immer/docs/freezing
+ */
+setAutoFreeze(process.env.NODE_ENV !== 'production');
 
 type TState = {
   router: RouterState;
