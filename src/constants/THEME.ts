581672ac940a6ad996f876ecf5dc3971d7f10222
Antonios Karagiannis <antokarag@gmail.com>
2019-11-14 17:25:30 -0700

    feat(theme constant): add mui theme

---
+++ src/constants/THEME.ts
@@ -0,0 +1,23 @@
+import { createMuiTheme, Theme } from '@material-ui/core';
+
+// style components Theme Object with Material UI theme combined
+// @see https://www.styled-components.com/docs/advanced#theming
+// @see https://material-ui.com/customization/default-theme/#default-theme
+const THEME: Theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Noto Serif JP, sans-serif'
+  },
+  palette: {
+    primary: {
+      main: '#82171d'
+    },
+    secondary: {
+      main: '#9a5d40'
+    },
+    text: {
+      primary: '#ffffffc0'
+    }
+  }
+});
+
+export { THEME };
