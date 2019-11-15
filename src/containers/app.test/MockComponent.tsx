ea10fea160ccc40afeef6c61595ad3eb482ffda1
Antonios Karagiannis <antokarag@gmail.com>
2019-11-14 17:26:54 -0700

    feat(app container): add mui theme provider
    * add the provider
    * add tests for it

--- src/containers/app.test/MockComponent.tsx
+++ src/containers/app.test/MockComponent.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { MockApolloConsumer } from './MockApolloConsumer';
+import { MockMUIThemeConsumer } from './MockMUIThemeConsumer';
 import { MockReduxContainer } from './MockReduxContainer';
 
 /**
- * a mock functional component which wraps both
- * mock consumer/container for convenient testing in one go
+ * a mock functional component which wraps all the
+ * mock consumers/containers for convenient testing in one go
  */
 const MockComponent: () => JSX.Element = (): JSX.Element => (
   <div>
     <MockApolloConsumer />
     <MockReduxContainer />
+    <MockMUIThemeConsumer />
   </div>
 );
 
---
+++ src/containers/app.test/MockMUIThemeConsumer.tsx
@@ -0,0 +1,23 @@
+import { Theme, useTheme } from '@material-ui/core';
+import * as React from 'react';
+
+/**
+ * a mock functional component which consumes the material ui theme
+ * @see https://material-ui.com/styles/advanced/#accessing-the-theme-in-a-component
+ */
+const MockMUIThemeConsumer: () => JSX.Element = (): JSX.Element => {
+  const theme: Theme = useTheme();
+
+  return (
+    <div
+      data-testid="mock-mui-theme-consumer"
+      data-palette-primary-main={theme.palette.primary.main}
+      data-palette-secondary-main={theme.palette.secondary.main}
+      data-palette-text-primary={theme.palette.text.primary}
+    >
+      MockMUIThemeConsumer
+    </div>
+  );
+};
+
+export { MockMUIThemeConsumer };
