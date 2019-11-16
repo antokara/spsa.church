50ed66643129c27a74cb5ce4b170cd866744d071
Antonios Karagiannis <antokarag@gmail.com>
2019-11-15 19:54:06 -0700

    refactor(menu open action): change structure
    * replace string constant with enum string
    * restructure directory/file
    * refactor imports

---
+++ src/constants/layout/EActionTypes.ts
@@ -0,0 +1,5 @@
+enum EActionTypes {
+  MENU_OPEN = 'LAYOUT.MENU_OPEN'
+}
+
+export { EActionTypes };
--- src/reducers/layout/layout.ts
+++ src/reducers/layout/layout.ts
@@ -1,12 +1,12 @@
 import { produce } from 'immer';
 import { Reducer } from 'redux';
 import { TAction } from 'src/actions/layout/menuOpen';
-import { MENU_OPEN } from 'src/constants/actionTypes/layout/MENU_OPEN';
+import { EActionTypes } from 'src/constants/layout/EActionTypes';
 import { defaultState, TState } from 'src/reducers/layout/defaultState';
 
 const layout: Reducer = produce((draft: TState, action: TAction) => {
   switch (action.type) {
-    case MENU_OPEN:
+    case EActionTypes.MENU_OPEN:
       draft.menuOpen = action.payload;
     default:
   }
