656941e44254d142046d8c0f1fcba60784859741
Antonios Karagiannis <antokarag@gmail.com>
2019-11-20 08:27:29 -0700

    WIP: offline support for graphql

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,6 +3,7 @@
  * the main container of the whole application
  */
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { ApolloClient } from 'apollo-client';
 import { ConnectedRouter } from 'connected-react-router';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
@@ -10,7 +11,7 @@ import { Provider } from 'react-redux';
 import { hot } from 'ReactHotLoader';
 import { Layout1 } from 'src/components/layouts/Layout1';
 import { THEME } from 'src/constants/THEME';
-import { ApolloClient } from 'src/helpers/ApolloClient';
+import { ApolloClientCreator } from 'src/helpers/ApolloClientCreator';
 import { history } from 'src/helpers/history';
 import { store } from 'src/helpers/store';
 import * as WebFont from 'webfontloader';
@@ -21,20 +22,42 @@ WebFont.load({
   }
 });
 
-const App: React.FunctionComponent = (): React.ReactElement<React.ReactNode> => (
-  <React.Fragment>
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <ApolloProvider client={ApolloClient}>
-          <MuiThemeProvider theme={THEME}>
-            <CssBaseline />
-            <Layout1 />
-          </MuiThemeProvider>
-        </ApolloProvider>
-      </ConnectedRouter>
-    </Provider>
-  </React.Fragment>
-);
+const App: React.FunctionComponent | null = (): React.ReactElement<
+  React.ReactNode
+> | null => {
+  // let apolloClient: ApolloClient<{}> | null = null;
+
+  const [state, setState] = useState(() => {
+    const initialState: { apolloClient: ApolloClient<{}> | null } = {};
+
+    return initialState;
+  });
+
+  // React.useLayoutEffect(() => {
+  //   ApolloClientCreator().then((client: ApolloClient<{}>) => {
+  //     apolloClient = client;
+  //   });
+  // });
+
+  if (props.apolloClient !== null) {
+    return (
+      <React.Fragment>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <ApolloProvider client={apolloClient}>
+              <MuiThemeProvider theme={THEME}>
+                <CssBaseline />
+                <Layout1 />
+              </MuiThemeProvider>
+            </ApolloProvider>
+          </ConnectedRouter>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+
+  return null;
+};
 
 const containerApp: React.FunctionComponent = hot(module)(App);
 
