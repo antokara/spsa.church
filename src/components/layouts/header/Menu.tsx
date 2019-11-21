7b242e129a437d54dcf65c87b218a143352e368e
Antonios Karagiannis <antokarag@gmail.com>
2019-11-20 18:48:34 -0700

    docs(menu component): annotate

--- src/components/layouts/header/Menu.tsx
+++ src/components/layouts/header/Menu.tsx
@@ -10,21 +10,35 @@ import * as getTheme from 'src/gql/theme/getTheme.gql';
 import { TData, TMenuEntry } from 'src/gql/theme/TData';
 import { TState } from 'src/reducers/defaultState';
 
+/**
+ * Header menu component.
+ *
+ * Renders the Menu Icon FAB which toggles the menu open state.
+ * It also renders the Menu in a Drawer
+ */
 const Menu: () => JSX.Element | null = (): JSX.Element | null => {
   const dispatch: Dispatch = useDispatch();
   const menuOpenAc: TActionCreator = bindActionCreators(menuOpen, dispatch);
   const menuOpenSt: boolean = useSelector(
     (state: TState) => state.layout.menuOpen
   );
+
+  // toggle the menu open state
   const onClick: () => TAction = (): TAction => menuOpenAc(!menuOpenSt);
+
+  // get the theme data
   const { loading, data } = useQuery<TData>(getTheme, {
     fetchPolicy: 'cache-first'
   });
 
+  // in case the gql is loading or there is no data, do not show the menu
   if (loading || !data) {
     return null;
   }
 
+  /**
+   * builds the list items for the menu
+   */
   const ListItems: JSX.Element[] = data.theme.headerMenu.menuEntries.map(
     (menuEntry: TMenuEntry) => (
       <ListItem
