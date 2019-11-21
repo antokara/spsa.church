27a8712e134af5af8fabcc237d919a2ebdf8c190
Antonios Karagiannis <antokarag@gmail.com>
2019-11-20 18:39:01 -0700

    feat(page loading component): add

---
+++ src/components/app/PageLoading.tsx
@@ -0,0 +1,32 @@
+import { Box, CircularProgress, Fade } from '@material-ui/core';
+import * as React from 'react';
+
+type TProps = {
+  visible?: boolean;
+};
+
+/**
+ * a page loading spinner that covers the whole page
+ * with absolute left/top positioning and fade in/out capabilities
+ */
+const PageLoading: (props: TProps) => JSX.Element = ({
+  visible = true
+}: TProps): JSX.Element => (
+  <Fade in={visible}>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      position="absolute"
+      top={0}
+      left={0}
+      width="100%"
+      height="100%"
+      bgcolor="background.paper"
+    >
+      <CircularProgress />
+    </Box>
+  </Fade>
+);
+
+export { PageLoading };
