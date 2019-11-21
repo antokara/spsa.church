71f68b9b0912a2cf93bb7f49f196084f92a7bd95
Antonios Karagiannis <antokarag@gmail.com>
2019-11-21 16:44:13 -0700

    test(menu open action): add

--- src/actions/layout/menuOpen.test.ts
+++ src/actions/layout/menuOpen.test.ts
@@ -1,3 +1,15 @@
+import { EActionTypes } from 'src/constants/layout/EActionTypes';
 import { menuOpen } from './menuOpen';
 
-describe('menuOpen component', () => {});
+describe('menuOpen action', () => {
+  it('is a function', () => {
+    expect(menuOpen).toBeInstanceOf(Function);
+  });
+
+  it('returns an action when called', () => {
+    expect(menuOpen(true)).toStrictEqual({
+      type: EActionTypes.MENU_OPEN,
+      payload: true
+    });
+  });
+});
