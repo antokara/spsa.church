6aded7c26bf4f4848f36d54baa93a2245975529f
Antonios Karagiannis <antokarag@gmail.com>
2019-11-22 08:12:37 -0700

    test(page loading): add

--- src/components/app/PageLoading.test.tsx
+++ src/components/app/PageLoading.test.tsx
@@ -6,6 +6,36 @@ describe('PageLoading component', () => {
   let rr: RenderResult;
   let node: ChildNode | null;
 
+  describe('with required props', () => {
+    beforeEach(() => {
+      rr = render(<PageLoading />);
+      node = rr.container.firstChild;
+    });
+
+    it('renders the component', () => {
+      expect(node).toMatchSnapshot();
+    });
+
+    describe('fade component', () => {
+      let c: ChildNode | null;
+      beforeEach(() => {
+        c = rr.queryByTestId('fade');
+      });
+
+      it('gets rendered', () => {
+        expect(c).toBeTruthy();
+      });
+
+      it('has opacity:1', () => {
+        expect(c).toHaveStyle('opacity: 1');
+      });
+    });
+
+    it('renders the circular progress', () => {
+      expect(rr.queryByTestId('circular-progress')).toBeTruthy();
+    });
+  });
+
   describe('with prop.visible:true', () => {
     beforeEach(() => {
       rr = render(<PageLoading visible={true} />);
