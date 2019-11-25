3e2c60d2f9a5dc75a32f2fc939f2595ad7cdf9f0
Antonios Karagiannis <antokarag@gmail.com>
2019-11-25 15:37:57 -0700

    refactor(layout component): use maxWidth constant
    * add maxWidth constant object
    * refactor layout/menu/home components to use it

---
+++ src/constants/layout/maxWidth.ts
@@ -0,0 +1,18 @@
+// the maximum width our layout should expand to
+const unit: string = 'px';
+const value: number = 1024;
+const property: string = `${value}${unit}`;
+
+type TMaxWidth = {
+  unit: string;
+  value: number;
+  property: string;
+};
+
+const maxWidth: TMaxWidth = {
+  unit,
+  value,
+  property
+};
+
+export { maxWidth };
