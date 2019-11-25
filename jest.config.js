9eefc5a70c5c5bfae2e5d0db6dde6946746dd9a4
Antonios Karagiannis <antokarag@gmail.com>
2019-11-25 15:14:56 -0700

    build(jest config): remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['.*.test/.*$'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
     }
   },
   setupFiles: ['<rootDir>/jest.setup.ts', 'jest-localstorage-mock'],
