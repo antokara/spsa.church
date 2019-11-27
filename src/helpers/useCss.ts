4c40151b90f4fcb25197ef4a9881f2080621bb64
Antonios Karagiannis <antokarag@gmail.com>
2019-11-27 13:13:25 -0700

    feat(usecss helper function): add maxW param

--- src/helpers/useCss.ts
+++ src/helpers/useCss.ts
@@ -5,15 +5,17 @@ import { maxWidth } from 'src/constants/layout/maxWidth';
  * a functional component hook that
  * returns the css value to use, depending on the window's width
  *
- * @param scalable css value to use when the window's width is under the maxWidth
+ * @param scalable css value to use when the window's width is under the maxW
  *                 usually, a dynamic value such as "vw"
- * @param fixed css value to use when the window's width is above the maxWidth
+ * @param fixed css value to use when the window's width is above the maxW
  *                 usually, a fixed value such as "10rem"
+ * @param maxW optional. Maximum width for the media query.
+ *                 Defaults to the constant maxWidth.property
  */
-const useCss: (scalable: string, fixed: string) => string = (
+const useCss: (scalable: string, fixed: string, maxW?: string) => string = (
   scalable: string,
-  fixed: string
-): string =>
-  useMediaQuery(`(max-width: ${maxWidth.property})`) ? scalable : fixed;
+  fixed: string,
+  maxW: string = maxWidth.property
+): string => (useMediaQuery(`(max-width: ${maxW})`) ? scalable : fixed);
 
 export { useCss };
