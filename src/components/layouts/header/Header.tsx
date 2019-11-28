a840bf29d4c80233325ace58fa45a120d7bacda3
Antonios Karagiannis <antokarag@gmail.com>
2019-11-28 10:06:36 -0700

    feat(generic page component): add

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { Menu } from 'src/components/layouts/header/menu/Menu';
+import { WideImage } from 'src/components/layouts/header/wideImage/WideImage';
 
-const Header: () => JSX.Element = (): JSX.Element => <Menu />;
+const Header: () => JSX.Element = (): JSX.Element => (
+  <>
+    <Menu />
+    <WideImage />
+  </>
+);
 
 export { Header };
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -2,25 +2,19 @@ import { useQuery } from '@apollo/react-hooks';
 import { Box, Grid } from '@material-ui/core';
 import * as React from 'react';
 import { Markdown } from 'src/components/shared/Markdown';
-import { Picture, TSource } from 'src/components/shared/Picture';
 import { Separator } from 'src/components/shared/Separator';
-import { maxWidth } from 'src/constants/layout/maxWidth';
 import * as getHome from 'src/gql/home/getHome.gql';
-import { TAsset, TData } from 'src/gql/home/TData';
-import { useCss } from 'src/helpers/useCss';
+import { TData } from 'src/gql/home/TData';
 import { Calendar } from './Calendar';
 
 /**
  * Home page component.
  *
- * Renders the home page with its Main Photo, Overlay text, content, etc.
+ * Renders the home page with its content, Calendar, etc.
  */
 const Home: () => JSX.Element | null = (): JSX.Element | null => {
   // get the home data
   const { loading, data } = useQuery<TData>(getHome);
-
-  // must be calculated every time
-  const overlayFontSize: string = useCss('3vw', '2rem');
 
   // in case the gql is loading or there is no data, do not show the page
   // TODO: add loader
@@ -28,42 +22,8 @@ const Home: () => JSX.Element | null = (): JSX.Element | null => {
     return null;
   }
 
-  const focalPointPortrait: string = 'fp-x=0.35&fp-y=0.35&fp-z=1';
-  const focalPointLandscape: string = 'fp-x=0.5&fp-y=0.5&fp-z=1';
-  const assets: TAsset[] = [data.home.photoPortrait, data.home.photoLandscape];
-
-  const sources: TSource[] = [
-    {
-      srcSet: `{url[0]}?${focalPointPortrait}&fit=crop&h=700&w=${maxWidth.value}`,
-      media: '(orientation: portrait)'
-    },
-    {
-      srcSet: `{url[1]}?${focalPointLandscape}&fit=crop&h=500&w=${maxWidth.value}`,
-      media: '(orientation: landscape)'
-    }
-  ];
-
   return (
     <Grid container={true}>
-      <Grid item={true} xs={12}>
-        <Box position="relative">
-          <Picture assets={assets} sources={sources} />
-          <Box
-            position="absolute"
-            fontSize={overlayFontSize}
-            maxWidth="45%"
-            bottom="5%"
-            right={0}
-            p={1}
-            textAlign="right"
-            color="white"
-            bgcolor="rgba(0, 0, 0, 0.5)"
-          >
-            {data.home.overlay}
-          </Box>
-          <Separator />
-        </Box>
-      </Grid>
       <Grid item={true} xs={12}>
         <Box position="relative">
           <Box p={2} overflow="hidden">
