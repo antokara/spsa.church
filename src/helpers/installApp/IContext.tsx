bb60287fc22b3611084f471dca4fa37233d3c88e
Antonios Karagiannis <antokarag@gmail.com>
2019-12-04 15:24:56 -0700

    refactor(install app icontext): optional props

--- src/helpers/installApp/IContext.tsx
+++ src/helpers/installApp/IContext.tsx
@@ -24,28 +24,28 @@ interface IContext {
    * where supported. holds the deferred prompt event object
    * @see https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
    */
-  deferredPrompt: BeforeInstallPromptEvent | undefined;
+  deferredPrompt?: BeforeInstallPromptEvent;
   /**
    * true, if we know that it is already installed, so we do not prompt for no reason
    * false, if we know it is not installed and undefined if we are not sure yet.
    */
-  alreadyInstalled: boolean | undefined;
+  alreadyInstalled?: boolean;
   /**
    * where supported. Describes the outcome of the Install Prompt.
    * So that if it just got installed, we can hide or change our content (ie. show thank you)
    */
-  outcome: EOutcome | undefined;
+  outcome?: EOutcome;
   /**
    * the current platform. So we can show either the install button or
    * just show installation instructions (ie. iOS)
    */
-  platform: EPlatform | undefined;
+  platform?: EPlatform;
   /**
    * where supported. If true, the application is running in standalone mode (not through the browser)
    * False, it is running through the browser and undefined if we are not sure yet.
    * @see https://developers.google.com/web/fundamentals/app-install-banners#detect-mode
    */
-  standalone: boolean | undefined;
+  standalone?: boolean;
 }
 
 const defaultState: IContext = {
