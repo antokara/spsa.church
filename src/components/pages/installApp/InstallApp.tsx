0df34f25c3df941758498e61146fe4f935b5cfa1
Antonios Karagiannis <antokarag@gmail.com>
2019-12-04 19:30:44 -0700

    feat(install app page): add dynamic content

--- src/components/pages/installApp/InstallApp.tsx
+++ src/components/pages/installApp/InstallApp.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/react-hooks';
-import { Box } from '@material-ui/core';
+import { Box, Button } from '@material-ui/core';
+import { default as IOsA2HS } from 'assets/iOsA2HS.svg';
+import { default as IOsShareSvg } from 'assets/iOsShare.svg';
 import * as React from 'react';
 import { NoInternet } from 'src/components/pages/noInternet/NoInternet';
 import { PageLoading } from 'src/components/shared/pageLoading/PageLoading';
@@ -8,9 +10,145 @@ import { imageSizes } from 'src/constants/layout/imageSizes';
 import * as getInstallApp from 'src/gql/installApp/getInstallApp.gql';
 import { TData } from 'src/gql/installApp/TData';
 import { Context } from 'src/helpers/installApp/Context';
-import { IContext } from 'src/helpers/installApp/IContext';
+import {
+  EInstalled,
+  EPlatform,
+  IContext
+} from 'src/helpers/installApp/IContext';
 type TProps = {
   id: string;
+};
+
+// TODO: remove when dynamic
+// tslint:disable-next-line:max-func-body-length
+const selectContent: (context: IContext) => JSX.Element = (
+  context: IContext
+): JSX.Element => {
+  if (context.standalone) {
+    // already inside the standalone PWA
+    // give instructions on how the user can share the app with others
+    return (
+      <div>
+        <p>You are already in the application.</p>
+        <p>Please share it with anyone who you think might be interested.</p>
+      </div>
+    );
+  } else {
+    // accessed through a browser
+    if (context.platform === EPlatform.iOS) {
+      // show installation instructions for iOS
+      return (
+        <div>
+          <p>To install the Application you need to</p>
+          <ol>
+            <li>
+              tap on the share <IOsShareSvg /> button
+            </li>
+            <li>
+              tap on the Add to Home Screen <IOsA2HS /> button
+            </li>
+          </ol>
+          <p>
+            After that, this application will be available in your Home Screen
+            for quick access
+          </p>
+        </div>
+      );
+    } else if (context.platform === EPlatform.supported) {
+      // installation prompt is supported but may not be available at this moment
+      if (context.deferredPrompt) {
+        // show the install button -> prompt
+        type TInstallResult = () => void;
+        let triggerInstallation: undefined | TInstallResult;
+        triggerInstallation = (): void => {
+          if (context.deferredPrompt) {
+            context.deferredPrompt
+              .prompt()
+              .then()
+              .catch();
+          }
+        };
+
+        return (
+          <div>
+            <p>
+              You may install the Application, for quick access and offline
+              usage!
+            </p>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={triggerInstallation}
+            >
+              Install
+            </Button>
+          </div>
+        );
+      } else if (context.installed === EInstalled.justInstalled) {
+        // thank the user for installing the app and inform them how they can access it
+        return (
+          <div>
+            <p>Thank you for Installing the Application!</p>
+            <p>
+              The Application can now be accessed from your Home Screen or App
+              Drawer for quick access
+            </p>
+          </div>
+        );
+      } else if (context.installed === EInstalled.alreadyInstalled) {
+        // inform the user that the app is already installed and how they can access it
+        return (
+          <div>
+            <p>You have already installed the application.</p>
+            <p>
+              The Application can be accessed from your Home Screen or App
+              Drawer for quick access
+            </p>
+          </div>
+        );
+      } else if (context.installed === EInstalled.maybeInstalled) {
+        // it is probably installed but we're not sure and we do not have the ability to show the install button -> prompt
+        // show instructions of how to manually install the app depending the browser
+        return (
+          <div>
+            <p>You have already installed the application in the past.</p>
+            <p>
+              The Application should be accessible from your Home Screen or App
+              Drawer for quick access.
+            </p>
+            <p>
+              If you cannot find the application in your Home Screen or App
+              Drawer
+            </p>
+            <ol>
+              <li>open your browser's menu</li>
+              <li>tap on the Add to Home Screen button</li>
+            </ol>
+          </div>
+        );
+      } else {
+        // it is not installed and we do not have the ability to show the install button -> prompt
+        // show instructions of how to manually install the app depending the browser
+      }
+    } else {
+      // probably app installation is not supported,
+      // just instruct user to add to home page using a generic method
+    }
+  }
+
+  return (
+    <div>
+      <p>To install the Application you need to</p>
+      <ol>
+        <li>open your browser's menu</li>
+        <li>tap on the Add to Home Screen button</li>
+      </ol>
+      <p>
+        After that, this application will be available in your Home Screen/App
+        Drawer for quick access
+      </p>
+    </div>
+  );
 };
 
 /**
@@ -46,33 +184,11 @@ const InstallApp: (props: TProps) => JSX.Element | null = ({
     );
   }
 
-  type TInstallResult = () => void;
-  let triggerInstallation: undefined | TInstallResult;
-  triggerInstallation = (): void => {
-    if (context.deferredPrompt) {
-      context.deferredPrompt
-        .prompt()
-        .then()
-        .catch();
-    }
-  };
-
-  return (
-    <Box px={2}>
-      {contents}
-      can install: {context.deferredPrompt ? 'yes' : 'no'}
-      <button onClick={triggerInstallation}>install!</button>
-      <br />
-      standalone:
-      {context.standalone ? 'yes' : 'no'}
-      <br />
-      installed:
-      {context.installed}
-      <br /> outcome: {context.outcome}
-      platform:
-      {context.platform}
-    </Box>
+  contents.push(
+    React.cloneElement(selectContent(context), { key: 'installApp' })
   );
+
+  return <Box px={2}>{contents}</Box>;
 };
 
 export { InstallApp };
