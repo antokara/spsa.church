04be7c1f119c055e40abb20eb515a4c6326753e2
Antonios Karagiannis <antokarag@gmail.com>
2019-12-05 15:15:08 -0700

    refactor(install app prompt): move

--- src/components/layouts/InstallAppPrompt.tsx
+++ src/components/layouts/InstallAppPrompt.tsx
@@ -1,11 +1,7 @@
 import { Box, Button, Grid, Slide } from '@material-ui/core';
 import * as React from 'react';
 import { Context } from 'src/helpers/installApp/Context';
-import {
-  EInstalled,
-  EPlatform,
-  IContext
-} from 'src/helpers/installApp/IContext';
+import { IContext } from 'src/helpers/installApp/IContext';
 
 /**
  * Install Application prompt component.
@@ -22,65 +18,55 @@ const InstallAppPrompt: () => JSX.Element | null = (): JSX.Element | null => {
     return null;
   }
 
-  // check if already shown/times
-
   // accessed through a browser
-  if (
-    context.platform === EPlatform.iOS ||
-    (context.platform === EPlatform.supported &&
-      (!context.installed || context.installed === EInstalled.no))
-  ) {
-    let onInstallClick: () => void = (): void => {
-      return;
-    };
-    if (context.nativePromptToInstall) {
-      onInstallClick = context.nativePromptToInstall;
-    }
-
-    const onLaterClick: () => void = (): void => {
-      if (context.setPromptVisibility) {
-        context.setPromptVisibility(false);
-      }
-    };
-
-    // show installation prompt when iOS or
-    // when platform supports installation and it's not installed
-    return (
-      <Box position="fixed" bottom="1em">
-        <Slide
-          direction="up"
-          in={context.isPromptVisible}
-          mountOnEnter={true}
-          unmountOnExit={true}
-        >
-          <Box padding={1} bgcolor="secondary.dark" boxShadow={2}>
-            <Grid container={true} spacing={2}>
-              <Grid item={true}>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={onInstallClick}
-                >
-                  Install App
-                </Button>
-              </Grid>
-              <Grid item={true}>
-                <Button
-                  color="secondary"
-                  variant="contained"
-                  onClick={onLaterClick}
-                >
-                  Later
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        </Slide>
-      </Box>
-    );
+  let onInstallClick: () => void = (): void => {
+    return;
+  };
+  if (context.nativePromptToInstall) {
+    onInstallClick = context.nativePromptToInstall;
   }
 
-  return null;
+  const onLaterClick: () => void = (): void => {
+    if (context.setPromptVisibility) {
+      context.setPromptVisibility(false);
+    }
+  };
+
+  // show installation prompt when iOS or
+  // when platform supports installation and it's not installed
+  return (
+    <Box position="fixed" bottom="1em">
+      <Slide
+        direction="up"
+        in={context.isPromptVisible}
+        mountOnEnter={true}
+        unmountOnExit={true}
+      >
+        <Box padding={1} bgcolor="secondary.dark" boxShadow={2}>
+          <Grid container={true} spacing={2}>
+            <Grid item={true}>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={onInstallClick}
+              >
+                Install App
+              </Button>
+            </Grid>
+            <Grid item={true}>
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={onLaterClick}
+              >
+                Later
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Slide>
+    </Box>
+  );
 };
 
 export { InstallAppPrompt };
