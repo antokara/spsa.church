b7f37c60f093c3e06fe99beba9b3960d529ba6cc
Antonios Karagiannis <antokarag@gmail.com>
2019-12-10 11:14:21 -0700

    fix(offline cache): add index.html to offline

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,67 @@
+workbox.LOG_LEVEL = 'debug';
+
 // @see https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.core#.skipWaiting
 workbox.core.skipWaiting();
 
 // @see https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.core#.clientsClaim
 workbox.core.clientsClaim();
 
+// TODO: refactor/move cache names to constants
+
+/**
+ * handle the SPA root directory request and
+ * any other non-file URL in the same domain
+ */
+// start by pre-caching our index html page
+caches
+  .open('index')
+  .then(cache => cache.add('/').catch())
+  .catch();
+/**
+ * custom matcher that only handles:
+ *  - "navigate" requests
+ *  - URLs that are not files (do not have a . in the pathname)
+ *  - URLs that are not the webpack dev server socket (do not have 'sockjs-node/info' in the pathname)
+ */
+const matchCb = ({ url, request }) =>
+  request.mode === 'navigate' &&
+  !url.pathname.includes('.') &&
+  !url.pathname.includes('sockjs-node/info');
+/**
+ * custom handler that attempts to load the pre-cached "/" request response and
+ * if not already there, returns the fetch response from network.
+ *
+ * the same logic applies to ANY url requested since the custom matcher above, white-lists them.
+ */
+const handlerCb = ({ event }) => {
+  return caches
+    .open('index')
+    .then(cache => {
+      return cache
+        .match(new Request('/'))
+        .then(response => response || fetch(event.request));
+    })
+    .catch();
+};
+/**
+ * register our custom match/route handler
+ */
+workbox.routing.registerRoute(matchCb, handlerCb);
+
 // cache CMS assets
 workbox.routing.registerRoute(
   new RegExp('https://images.takeshape.io/'),
+  // @see https://developers.google.com/web/tools/workbox/reference-docs/latest/workbox.strategies.StaleWhileRevalidate.html
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'takeshape-cms-assets'
+    cacheName: 'takeshape-cms-assets',
+    // @see https://w3c.github.io/ServiceWorker/#dictdef-cachequeryoptions
+    matchOptions: {
+      // that's required because the CDN uses the vary header and it's not worth trying to
+      // match it here. To avoid using this flag, we would need to modify
+      // src/helpers/preloader/preloadAsset.ts to use the same headers as the browser does.
+      // ie. add the "accept: */*" header
+      ignoreVary: true
+    }
   })
 );
 
