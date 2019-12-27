2bfbda3cb6ddf844fc778f35dd8774c356b3972a
Antonios Karagiannis <antokarag@gmail.com>
2019-12-27 10:13:37 -0700

    refactor(docker db): move table names to own file

---
+++ docker/db/constants.js
@@ -0,0 +1,9 @@
+exports.tableNames = {
+  coreStore: 'core_store',
+  admin: 'strapi_administrator',
+  uploadFile: 'upload_file',
+  uploadFileMorph: 'upload_file_morph',
+  usersPermissionsPermission: 'users-permissions_permission',
+  usersPermissionsRole: 'users-permissions_role',
+  usersPermissionsUser: 'users-permissions_user'
+};
--- docker/db/migrations/20191224113506_init.js
+++ docker/db/migrations/20191224113506_init.js
@@ -1,12 +1,5 @@
 require('../loadEnv');
-
-const coreStoreTableName = 'core_store';
-const adminTableName = 'strapi_administrator';
-const uploadFileTableName = 'upload_file';
-const uploadFileMorphTableName = 'upload_file_morph';
-const usersPermissionsPermissionTableName = 'users-permissions_permission';
-const usersPermissionsRoleTableName = 'users-permissions_role';
-const usersPermissionsUserTableName = 'users-permissions_user';
+const { tableNames } = require('../constants');
 let data;
 let lastId;
 let indexPrefix;
@@ -17,7 +10,7 @@ exports.up = async knex => {
 
   // core_store
   // columns
-  await knex.schema.createTable(coreStoreTableName, function(table) {
+  await knex.schema.createTable(tableNames.coreStore, function(table) {
     table.increments('id');
     table.string('key', 255);
     table.text('value');
@@ -29,32 +22,32 @@ exports.up = async knex => {
   // indices
   // @see https://github.com/knex/knex/issues/203
   await knex.raw(
-    `CREATE INDEX search_${coreStoreTableName}_environment ON public.${coreStoreTableName} USING gin (environment gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.coreStore}_environment ON public.${tableNames.coreStore} USING gin (environment gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${coreStoreTableName}_key ON public.${coreStoreTableName} USING gin (key gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.coreStore}_key ON public.${tableNames.coreStore} USING gin (key gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${coreStoreTableName}_tag ON public.${coreStoreTableName} USING gin (tag gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.coreStore}_tag ON public.${tableNames.coreStore} USING gin (tag gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${coreStoreTableName}_type ON public.${coreStoreTableName} USING gin (type gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.coreStore}_type ON public.${tableNames.coreStore} USING gin (type gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${coreStoreTableName}_value ON public.${coreStoreTableName} USING gin (value gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.coreStore}_value ON public.${tableNames.coreStore} USING gin (value gin_trgm_ops);`
   );
 
   // data
   ({ data, lastId } = require('../data/20191224113506_init/coreStore'));
-  await knex(coreStoreTableName).insert(data);
+  await knex(tableNames.coreStore).insert(data);
   // auto-increment set
   await knex.raw(
-    `SELECT setval('${coreStoreTableName}_id_seq'::regclass, ${lastId});`
+    `SELECT setval('${tableNames.coreStore}_id_seq'::regclass, ${lastId});`
   );
 
   // strapi_administrator
   // columns
-  await knex.schema.createTable(adminTableName, table => {
+  await knex.schema.createTable(tableNames.admin, table => {
     table.increments('id');
     table
       .string('username', 255)
@@ -67,22 +60,22 @@ exports.up = async knex => {
   });
   // indices
   await knex.raw(
-    `CREATE INDEX search_${adminTableName}_resetpasswordtoken ON public.${adminTableName} USING gin ("resetPasswordToken" gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.admin}_resetpasswordtoken ON public.${tableNames.admin} USING gin ("resetPasswordToken" gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${adminTableName}_username ON public.${adminTableName} USING gin (username gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.admin}_username ON public.${tableNames.admin} USING gin (username gin_trgm_ops);`
   );
   // data
   ({ data, lastId } = require('../data/20191224113506_init/admin'));
-  await knex(adminTableName).insert(data);
+  await knex(tableNames.admin).insert(data);
   // auto-increment set
   await knex.raw(
-    `SELECT setval('${adminTableName}_id_seq'::regclass, ${lastId});`
+    `SELECT setval('${tableNames.admin}_id_seq'::regclass, ${lastId});`
   );
 
   // upload_file
   // columns
-  await knex.schema.createTable(uploadFileTableName, table => {
+  await knex.schema.createTable(tableNames.uploadFile, table => {
     table.increments('id');
     table.string('name', 255).notNullable();
     table.string('hash', 255).notNullable();
@@ -97,30 +90,30 @@ exports.up = async knex => {
   });
   // indices
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_ext ON public.${uploadFileTableName} USING gin (ext gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_ext ON public.${tableNames.uploadFile} USING gin (ext gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_hash ON public.${uploadFileTableName} USING gin (hash gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_hash ON public.${tableNames.uploadFile} USING gin (hash gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_mime ON public.${uploadFileTableName} USING gin (mime gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_mime ON public.${tableNames.uploadFile} USING gin (mime gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_name ON public.${uploadFileTableName} USING gin (name gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_name ON public.${tableNames.uploadFile} USING gin (name gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_provider ON public.${uploadFileTableName} USING gin (provider gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_provider ON public.${tableNames.uploadFile} USING gin (provider gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_sha256 ON public.${uploadFileTableName} USING gin (sha256 gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_sha256 ON public.${tableNames.uploadFile} USING gin (sha256 gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileTableName}_url ON public.${uploadFileTableName} USING gin (url gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFile}_url ON public.${tableNames.uploadFile} USING gin (url gin_trgm_ops);`
   );
 
   // upload_file_morph
   // columns
-  await knex.schema.createTable(uploadFileMorphTableName, table => {
+  await knex.schema.createTable(tableNames.uploadFileMorph, table => {
     table.increments('id');
     table.integer('upload_file_id');
     table.integer('related_id');
@@ -129,84 +122,87 @@ exports.up = async knex => {
   });
   // indices
   await knex.raw(
-    `CREATE INDEX search_${uploadFileMorphTableName}_field ON public.${uploadFileMorphTableName} USING gin (field gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFileMorph}_field ON public.${tableNames.uploadFileMorph} USING gin (field gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX search_${uploadFileMorphTableName}_related_type ON public.${uploadFileMorphTableName} USING gin (related_type gin_trgm_ops);`
+    `CREATE INDEX search_${tableNames.uploadFileMorph}_related_type ON public.${tableNames.uploadFileMorph} USING gin (related_type gin_trgm_ops);`
   );
 
   // users-permissions_permission
   // columns
-  await knex.schema.createTable(usersPermissionsPermissionTableName, table => {
-    table.increments('id');
-    table.string('type', 255).notNullable();
-    table.string('controller', 255).notNullable();
-    table.string('action', 255).notNullable();
-    table.boolean('enabled').notNullable();
-    table.string('policy', 255);
-    table.integer('role');
-  });
+  await knex.schema.createTable(
+    tableNames.usersPermissionsPermission,
+    table => {
+      table.increments('id');
+      table.string('type', 255).notNullable();
+      table.string('controller', 255).notNullable();
+      table.string('action', 255).notNullable();
+      table.boolean('enabled').notNullable();
+      table.string('policy', 255);
+      table.integer('role');
+    }
+  );
   // indices
-  indexPrefix = `search_${usersPermissionsPermissionTableName}_`.replace(
+  indexPrefix = `search_${tableNames.usersPermissionsPermission}_`.replace(
     '-',
     '_'
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}action" ON public."${usersPermissionsPermissionTableName}" USING gin (action gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}action" ON public."${tableNames.usersPermissionsPermission}" USING gin (action gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}controller" ON public."${usersPermissionsPermissionTableName}" USING gin (controller gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}controller" ON public."${tableNames.usersPermissionsPermission}" USING gin (controller gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}policy" ON public."${usersPermissionsPermissionTableName}" USING gin (policy gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}policy" ON public."${tableNames.usersPermissionsPermission}" USING gin (policy gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}type" ON public."${usersPermissionsPermissionTableName}" USING gin (type gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}type" ON public."${tableNames.usersPermissionsPermission}" USING gin (type gin_trgm_ops);`
   );
   // data
   ({
     data,
     lastId
   } = require('../data/20191224113506_init/usersPermissionsPermission'));
-  await knex(usersPermissionsPermissionTableName).insert(data);
+  await knex(tableNames.usersPermissionsPermission).insert(data);
   // auto-increment set
   await knex.raw(
-    `SELECT setval('${usersPermissionsPermissionTableName}_id_seq'::regclass, ${lastId});`
+    `SELECT setval('${tableNames.usersPermissionsPermission}_id_seq'::regclass, ${lastId});`
   );
 
   // users-permissions_role
   // columns
-  await knex.schema.createTable(usersPermissionsRoleTableName, table => {
+  await knex.schema.createTable(tableNames.usersPermissionsRole, table => {
     table.increments('id');
     table.string('name', 255).notNullable();
     table.string('description', 255);
     table.string('type', 255).unique();
   });
   // indices
-  indexPrefix = `search_${usersPermissionsRoleTableName}_`.replace('-', '_');
+  indexPrefix = `search_${tableNames.usersPermissionsRole}_`.replace('-', '_');
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}description" ON public."${usersPermissionsRoleTableName}" USING gin (description gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}description" ON public."${tableNames.usersPermissionsRole}" USING gin (description gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}name" ON public."${usersPermissionsRoleTableName}" USING gin (name gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}name" ON public."${tableNames.usersPermissionsRole}" USING gin (name gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}type" ON public."${usersPermissionsRoleTableName}" USING gin (type gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}type" ON public."${tableNames.usersPermissionsRole}" USING gin (type gin_trgm_ops);`
   );
   // data
   ({
     data,
     lastId
   } = require('../data/20191224113506_init/usersPermissionsRole'));
-  await knex(usersPermissionsRoleTableName).insert(data);
+  await knex(tableNames.usersPermissionsRole).insert(data);
   // auto-increment set
   await knex.raw(
-    `SELECT setval('${usersPermissionsRoleTableName}_id_seq'::regclass, ${lastId});`
+    `SELECT setval('${tableNames.usersPermissionsRole}_id_seq'::regclass, ${lastId});`
   );
 
   // users-permissions_user
   // columns
-  await knex.schema.createTable(usersPermissionsUserTableName, table => {
+  await knex.schema.createTable(tableNames.usersPermissionsUser, table => {
     table.increments('id');
     table
       .string('username', 255)
@@ -222,33 +218,33 @@ exports.up = async knex => {
     table.timestamps(true, true);
   });
   // indices
-  indexPrefix = `search_${usersPermissionsUserTableName}_`.replace('-', '_');
+  indexPrefix = `search_${tableNames.usersPermissionsUser}_`.replace('-', '_');
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}provider" ON public."${usersPermissionsUserTableName}" USING gin (provider gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}provider" ON public."${tableNames.usersPermissionsUser}" USING gin (provider gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}resetpasswordtoken" ON public."${usersPermissionsUserTableName}" USING gin ("resetPasswordToken" gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}resetpasswordtoken" ON public."${tableNames.usersPermissionsUser}" USING gin ("resetPasswordToken" gin_trgm_ops);`
   );
   await knex.raw(
-    `CREATE INDEX "${indexPrefix}username" ON public."${usersPermissionsUserTableName}" USING gin (username gin_trgm_ops);`
+    `CREATE INDEX "${indexPrefix}username" ON public."${tableNames.usersPermissionsUser}" USING gin (username gin_trgm_ops);`
   );
 };
 
 exports.down = async knex => {
   // core_store
-  await knex.schema.dropTable(coreStoreTableName);
+  await knex.schema.dropTable(tableNames.coreStore);
   // strapi_administrator
-  await knex.schema.dropTable(adminTableName);
+  await knex.schema.dropTable(tableNames.admin);
   // upload_file
-  await knex.schema.dropTable(uploadFileTableName);
+  await knex.schema.dropTable(tableNames.uploadFile);
   // upload_file_morph
-  await knex.schema.dropTable(uploadFileMorphTableName);
+  await knex.schema.dropTable(tableNames.uploadFileMorph);
   // users-permissions_permission
-  await knex.schema.dropTable(usersPermissionsPermissionTableName);
+  await knex.schema.dropTable(tableNames.usersPermissionsPermission);
   // users-permissions_role
-  await knex.schema.dropTable(usersPermissionsRoleTableName);
+  await knex.schema.dropTable(tableNames.usersPermissionsRole);
   // users-permissions_user
-  await knex.schema.dropTable(usersPermissionsUserTableName);
+  await knex.schema.dropTable(tableNames.usersPermissionsUser);
   // @see https://www.postgresql.org/docs/9.1/sql-dropextension.html
   await knex.raw('DROP EXTENSION pg_trgm CASCADE;');
 };
